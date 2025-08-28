c745ab6c8a544d2cd11375bf62bd31497e63cad0
Esli Rodrigues <eslicdm1@gmail.com>
2025-08-28 20:28:53 -0300

    Adding init

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    """
-    Manages application settings and environment variables.
-    """
     HF_API_TOKEN: str = ""
     HF_API_URL: str = "https://api-inference.huggingface.co/models/facebook/bart-large-mnli"
 
